84e0d62091f77189258b7cdd1a6d5c0f539b4507
IferiaEzekiel <ezekiel.iferia@gmail.com>
2023-06-16 01:45:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle")
+#include <stdio.h>
+/*
+ * Printing a line of strings with puts
+ * return: 0 (sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
